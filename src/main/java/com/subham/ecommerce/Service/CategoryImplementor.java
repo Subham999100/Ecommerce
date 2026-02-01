b82b9359c118ec5a09bc52b594bfc68e90103a29
Subham Patnaik <patnaiksubham30@gmail.com>
2026-02-01 19:06:06 +0530

    add deleted category

--- src/main/java/com/subham/ecommerce/Service/CategoryImplementor.java
+++ src/main/java/com/subham/ecommerce/Service/CategoryImplementor.java
@@ -1,7 +1,11 @@
 package com.subham.ecommerce.Service;
 
 import com.subham.ecommerce.Model.Category;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,5 +24,13 @@ public class CategoryImplementor implements  CategoryService{
          category.setCategoryId(id++);
          categories.add(category);
          return "Category added Succuesfully";
+    }
+
+    @Override
+    public String deletCategory(Long categoryId) {
+        Category category=categories.stream().filter(c->c.getCategoryId().equals(categoryId)).findFirst()
+                .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND,"Resource Not Found"));
+         categories.remove(category);
+         return "categoryid is deleted Succesfully"+categoryId;
     }
 }
--- src/main/java/com/subham/ecommerce/Service/CategoryService.java
+++ src/main/java/com/subham/ecommerce/Service/CategoryService.java
@@ -8,4 +8,6 @@ public interface CategoryService {
     List<Category> getAllCategories();
 
     String CreateCategories(Category category);
+
+    String deletCategory(Long categoryId);
 }
