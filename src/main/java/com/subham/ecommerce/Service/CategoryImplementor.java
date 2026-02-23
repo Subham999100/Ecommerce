f99a9aeff034bf8cc34349abe9548ea97f883bf3
Subham Patnaik <patnaiksubham30@gmail.com>
2026-02-23 18:54:24 +0530

    add

--- src/main/java/com/subham/ecommerce/Service/CategoryImplementor.java
+++ src/main/java/com/subham/ecommerce/Service/CategoryImplementor.java
@@ -32,26 +32,18 @@ public class CategoryImplementor implements  CategoryService{
 
     @Override
     public String deletCategory(Long categoryId) {
-        List<Category> categories = categoryRepository.findAll();
-        Category category=categories.stream().filter(c->c.getCategoryId().equals(categoryId)).findFirst()
-                .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND,"Resource Not Found"));
-         categoryRepository.delete(category);
+         Category  savedCategory = categoryRepository.findById(categoryId)
+                 .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND, " Category Not Found"));
+         categoryRepository.delete(savedCategory);
          return "categoryid is deleted Succesfully"+categoryId;
     }
 
 
     @Override
     public Category updateCategory(Category category, Long categoryId) {
-        List<Category> categories = categoryRepository.findAll();
-        Optional<Category> optionalCategory =categories.stream().filter(c->c.getCategoryId().equals(categoryId))
-                .findFirst();
-        if(optionalCategory.isPresent()){
-            Category existingCategory=optionalCategory.get();
-            existingCategory.setCategoryName(category.getCategoryName());
-            Category savedCategory=categoryRepository.save(category);
-            return savedCategory;
-        }else{
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND,"Category Not Found");
-        }
+        Category savedCategory=categoryRepository.findById(categoryId)
+                .orElseThrow(()->new ResponseStatusException(HttpStatus.NOT_FOUND,"Category Not Found"));
+        savedCategory.setCategoryName(category.getCategoryName());
+        return categoryRepository.save(savedCategory);
     }
 }
